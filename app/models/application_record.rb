d2b84dace2608527b6bf13a7db10042a15ded40c
Tom Leger <tomleger@gmail.com>
2022-08-15 14:01:01 -0400

    setting up for heroku

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+
+
+[POST] api/users: "users#create" (signup),
+[POST] api/session: "sessions#create" (login),
+[DELETE] api/session: "sessions#destroy" (logout)
